16d94615e64fd71ae5b9b99dd44392ffb057983f
Mahdi <esmailiyan.mahdi.com>
2025-02-17 13:56:22 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+class Game:
+    def __init__(self, database):
+        pass
+    def update(self, result):
+        pass
+    def filter(self):
+        pass
+    def suggest(self):
+        pass
+    def play(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
